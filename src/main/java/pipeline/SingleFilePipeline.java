5a79f1212209db26c1f2cf51c962495446567705
yangxuhao <xuhao.yang@cafintech.com>
2017-04-26 20:42:10 +0800

    ip138

---
+++ src/main/java/pipeline/SingleFilePipeline.java
@@ -0,0 +1,36 @@
+package pipeline;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by yangxvhao on 17-4-26.
+ */
+public class SingleFilePipeline implements Pipeline {
+
+    private final String path;
+
+    public SingleFilePipeline(String path){
+        this.path=path;
+    }
+
+    Logger logger= LoggerFactory.getLogger(SingleFilePipeline.class);
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+        try {
+            FileWriter fw=new FileWriter(path,true);
+            fw.write(resultItems.get("result").toString()+"\n");
+            logger.info("保存到文件....."+resultItems.get("result").toString());
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/until/UserAgentEnum.java
@@ -0,0 +1,19 @@
+package until;
+
+/**
+ * Created by yangxvhao on 17-4-25.
+ */
+public enum UserAgentEnum {
+    
+    USER_AGENT_ENUM("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36");
+    
+    private final String value;
+    
+    UserAgentEnum(String value){
+        this.value = value;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+}
